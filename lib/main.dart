42810c4d75d15358ccba1208e4fed5157934efef
10969ipan <arfianirfan48@gmail.com>
2026-04-19 01:57:33 +0700

    Selesai materi pertemuan 5 - Parsing Data dan Navigasi

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'baris_kolom.dart';
+import 'halaman_pertama.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Latihan Flutter',
-      home: BarisKolom(),
+      home: HalamanPertama(),
     );
   }
 }
